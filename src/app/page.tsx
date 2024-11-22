e1775e5fd85fe74b3f3f9968a53480d4d6e32901
Samueelljr <samueldejesus705@gmail.com>
2024-11-21 21:34:58 -0300

    Finalização da main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import LogoToYou from '../../public/LogoToYou.svg'
 import ImageHeader from './components/home/ImageHeader';
 import './style/home.css'
-import Computador from '../../public/Computador.svg';
 import Planos from '../app/components/Planos/index';
-import MulherComCelular from '../../public/MulherComCelular.svg'
 import Faq from './components/Faq';
 
 export default function Home() {
@@ -28,25 +25,55 @@ export default function Home() {
       <main>
         <h1>How It Works</h1>
 
+        <input type='image' className='bolinhas' src="Bolinhas.svg" alt="bolinhas" />
+
         <div className="info-container">
           <div className='info1-container' >
             <div className='info1'>
               <h5>Create Your Page</h5>
 
-              <p>Enter your information, select your closest friends, and personalize your friendship tree.</p>
+              <input type="image" className='risco1' src="Risco.svg" alt="risco" />
+
+              <p>Enter your information, select your closest friends, and <br /> personalize your friendship tree.</p>
             </div>
           </div>
 
           <div className="info2-container">
             <div className='info2'>
-              <img className='computador' src="Computador.svg" alt="Computador" />
+              <input type="image" className='computador' src="Computador.svg" alt="Computador" />
 
               <div className="info2-text">
 
                 <h5>Activate and Share</h5>
 
-                <p>Complete the payment and receive a link and QR code to easily share with your friends.</p>
+                <input type="image" className='risco2' src="Risco.svg" alt="risco" />
+
+                <p>Complete the payment and receive a link and QR code to breasily share with your friends.</p>
               </div>
+            </div>
+          </div>
+
+          <div className='info3-container' >
+            <div className='info3'>
+              <input type="image" className='settings' src="Settings.svg" alt="settings" />
+
+              <div className="info3-text">
+                <h5>Details That Make a Difference</h5>
+
+                <input type="image" className='risco3' src="Risco.svg" alt="risco" />
+
+                <p>Your friends fill out a form with preferences and <br /> memories – creating an exclusive mural of your best <br /> moments together.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='info4-container' >
+            <div className='info4'>
+              <h5>Automatic Reminders and <br /> Suggestions</h5>
+
+              <input type="image" className='risco4' src="Risco.svg" alt="risco" />
+
+              <p>Receive special notifications for birthdays and <br /> interests, with gift ideas and actions to strengthen <br /> these connections even more!</p>
             </div>
           </div>
         </div>
@@ -58,7 +85,7 @@ export default function Home() {
         <div className='text-section'>
           <h1>Get Started Now</h1>
 
-          <p>Don’t let your friendships stay only on social media. With 4yoou, create an exclusive space to celebrate and remember each special moment with the people who matter most.</p>
+          <p>Don't let your friendships stay only on social media. With 4yoou, create an exclusive space to celebrate and remember each special moment with the people who matter most.</p>
 
           <button className='btn-section'>
             Start Creating Your Friendship Site
